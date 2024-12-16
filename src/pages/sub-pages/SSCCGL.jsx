bcfb900ba3f40b9093b07acba232591c7b8257f5
manoharmeena12345 <manoharmeena245@gmail.com>
2024-12-16 18:09:42 +0530

    width fixed

--- src/pages/sub-pages/SSCCGL.jsx
+++ src/pages/sub-pages/SSCCGL.jsx
@@ -21,7 +21,7 @@ const SSCCGL = () => {
         title="SSC CGL - The WiNNERS Institute"
         canonicalLink="/ssc-cgl"
       />
-      <div className="container w-[100%] sm:w-[100%] mx-auto lg:px-6 sm:px-0 py-4 mt-[82px] relative">
+      <div className="container w-[80%] sm:w-[100%] mx-auto lg:px-6 sm:px-0 py-4 mt-[82px] relative">
         {/* Breadcrumbs */}
         <nav className="text-sm mb-4" aria-label="Breadcrumb">
           <ol className="list-none p-0 inline-flex">
